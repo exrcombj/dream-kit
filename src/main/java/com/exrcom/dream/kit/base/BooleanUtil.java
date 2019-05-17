b7f1252d7624c9370f2c8f237497b8bdb2225d59
Yufei Ren <ren.yufei@outlook.com>
2019-05-17 22:18:51 +0800

    update for BooleanUitl

--- src/main/java/com/exrcom/dream/kit/base/BooleanUtil.java
+++ src/main/java/com/exrcom/dream/kit/base/BooleanUtil.java
@@ -1,5 +1,7 @@
 package com.exrcom.dream.kit.base;
 
+import org.apache.commons.lang3.BooleanUtils;
+
 /**
  * 1. 从String(true/false, yes/no)，转换为Boolean或boolean
  *
@@ -27,6 +29,43 @@ public class BooleanUtil {
      */
     public static Boolean toBooleanObject(String str, Boolean defaultValue) {
         return str != null ? Boolean.valueOf(str) : defaultValue;
+    }
+
+    /**
+     * wrapper for apache common BooleanUtils.toBooleanObject
+     * 支持true/false, on/off, y/n, yes/no的转换, str为空或无法分析时返回null
+     */
+    public static Boolean parseString(String str) {
+        return BooleanUtils.toBooleanObject(str);
+    }
+
+    /**
+     * wrapper for apache common BooleanUtils.toBooleanDefaultIfNull
+     * 支持true/false,on/off, y/n, yes/no的转换, str为空或无法分析时返回defaultValue
+     */
+    public static Boolean parseString(String str, Boolean defaultValue) {
+        return BooleanUtils.toBooleanDefaultIfNull(BooleanUtils.toBooleanObject(str), defaultValue);
+    }
+
+    /**
+     * wrapper for apache common BooleanUtils.negate
+     */
+    public static Boolean negate(final Boolean bool) {
+        return BooleanUtils.negate(bool);
+    }
+
+    /**
+     * wrapper for apache common BooleanUtils.and
+     */
+    public static boolean and(final boolean... array) {
+        return BooleanUtils.and(array);
+    }
+
+    /**
+     * wrapper for apache common BooleanUtils.or
+     */
+    public static boolean or(final boolean... array) {
+        return BooleanUtils.or(array);
     }
 
 }
