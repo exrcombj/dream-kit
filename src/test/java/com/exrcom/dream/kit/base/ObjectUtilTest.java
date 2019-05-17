4d972d6674c439391800dcce180de1b84ca18960
Yufei Ren <ren.yufei@outlook.com>
2019-05-18 00:20:36 +0800

    update ObjectUtil

--- src/test/java/com/exrcom/dream/kit/base/ObjectUtilTest.java
+++ src/test/java/com/exrcom/dream/kit/base/ObjectUtilTest.java
@@ -1,7 +1,10 @@
 package com.exrcom.dream.kit.base;
 
+import com.google.common.collect.Lists;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ObjectUtilTest {
 
@@ -11,6 +14,23 @@ public class ObjectUtilTest {
         int hash2 = ObjectUtil.hashCode("a", "a");
         System.out.printf("hash1: %d, hash2: %d", hash1, hash2);
         Assertions.assertThat(hash1 - hash2).isEqualTo(1);
+
+    }
+
+    @Test
+    public void testToPrettyString() {
+        assertThat(ObjectUtil.toPrettyString(null)).isEqualTo("null");
+        assertThat(ObjectUtil.toPrettyString(1)).isEqualTo("1");
+        assertThat(ObjectUtil.toPrettyString(new int[] {1, 2, 3})).isEqualTo("[1, 2, 3]");
+        assertThat(ObjectUtil.toPrettyString(new long[] { 1, 2 })).isEqualTo("[1, 2]");
+        assertThat(ObjectUtil.toPrettyString(new double[] { 1.1d, 2.2d })).isEqualTo("[1.1, 2.2]");
+        assertThat(ObjectUtil.toPrettyString(new float[] { 1.1f, 2.2f })).isEqualTo("[1.1, 2.2]");
+        assertThat(ObjectUtil.toPrettyString(new boolean[] { true, false })).isEqualTo("[true, false]");
+
+        assertThat(ObjectUtil.toPrettyString(Lists.newArrayList("1", "2"))).isEqualTo("{1,2}");
+
+
+
 
     }
 }
